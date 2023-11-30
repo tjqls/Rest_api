1854437275625071bd7849d3f197b40b73c54cd2
tjqls <tjqls2013@gmail.com>
2023-11-30 17:55:44 +0900

    Rest_api 공부2

--- src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/rest_api/boundedContext/member/controller/MemberController.java
@@ -1,7 +1,37 @@
 package com.ll.rest_api.boundedContext.member.controller;
 
-import org.springframework.stereotype.Controller;
+import com.ll.rest_api.boundedContext.member.entity.Member;
+import com.ll.rest_api.boundedContext.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
 public class MemberController {
-}
+    private final MemberService memberService;
+
+    @Getter
+    @AllArgsConstructor
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/login")
+        public Member login(@Valid LoginRequest loginRequest){
+            return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
+        }
+    }
